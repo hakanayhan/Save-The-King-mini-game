bb72b237faf5c77b27d49be770900cefa07cb7dc
DESKTOP-4U0FALR\Hakan <hakanayhnn@gmail.com>
2025-01-19 21:24:04 +0300

    add Set Color function to set the color of the circle

--- Assets/Scripts/Tile.cs
+++ Assets/Scripts/Tile.cs
@@ -7,12 +7,27 @@ public class Tile : MonoBehaviour
     private int x, y;
     private GridManager gridManager;
     public GridManager.Color color;
+    [SerializeField] private GameObject circle;
+
+    private Dictionary<GridManager.Color, Color> colorMapping = new Dictionary<GridManager.Color, Color>
+        {
+            { GridManager.Color.Red, Color.red },
+            { GridManager.Color.Blue, Color.blue },
+            { GridManager.Color.Green, Color.green }
+        };
+
     public void Initialize(int x, int y, GridManager manager, GridManager.Color color)
     {
         this.x = x;
         this.y = y;
         this.gridManager = manager;
         this.color = color;
+        SetColor();
+    }
+
+    private void SetColor()
+    {
+        circle.GetComponent<SpriteRenderer>().color = colorMapping[color];
     }
 
     private void OnMouseDown()
