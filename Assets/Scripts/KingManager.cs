238496ac1bee124d60bf15f2306405d1f35946f4
DESKTOP-4U0FALR\Hakan <hakanayhnn@gmail.com>
2025-01-20 03:17:45 +0300

    add King Manager script to check if the king is drowning

---
+++ Assets/Scripts/KingManager.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KingManager : MonoBehaviour
+{
+    private float contactTime = 0f;
+    public float requiredTime = 5f;
+    private int objectsInWater = 0;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.layer == LayerMask.NameToLayer("Water"))
+        {
+            objectsInWater++;
+
+            if (objectsInWater == 1)
+            {
+                contactTime = 0f;
+            }
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.layer == LayerMask.NameToLayer("Water"))
+        {
+            objectsInWater--;
+
+            if (objectsInWater <= 0)
+            {
+                objectsInWater = 0;
+                contactTime = 0f;
+            }
+        }
+    }
+
+    void Update()
+    {
+        if (objectsInWater > 0)
+        {
+            contactTime += Time.deltaTime;
+
+            if (contactTime >= requiredTime)
+            {
+                TriggerEvent();
+                contactTime = 0f;
+            }
+        }
+    }
+
+    void TriggerEvent()
+    {
+        
+    }
+}
